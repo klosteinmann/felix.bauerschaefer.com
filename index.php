ce958eb69463a06172a1cbe70804941bc978a533
Klosteinmann <felix.bauerschaefer@mail.de>
2021-01-15 01:38:18 +0100

    inital commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php>
+  !<!DOCTYPE html>
+  <html lang="de">
+  <head>
+    <meta charset="utf-8" />
+    <title>F.B. kleine Webseite</title>
+  </head>
+<body>
+  <header>
+      <ul>
+        <li><a href="#losgehts">Los gehts></a></li>
+        <li><aPortfolio und so</li>
+        <li>vielleicht ein Blog</li>
+        <li>hier noch ein paar Fotos</li>
+        <li>meine Arbeit</li>
+        <li>bist du nicht auch ein Einhorn</li>
+        <li>Videos und so</li>
+        <li>Druckereck</li>
+        <li>kleiner Shop</li>
+        <li>wo findet man mich noch so?</li>
+        <li>Rechtliches</li>
+      </ul>
+  </header>
+  <main>
+    <p>
+      „Hey mein Name ist Felix.” So in der Art sah meine Biografie im Internet die letzten Jahre aus. Wird ja immer seltner das es sowas hier gibt, lasst es uns wieder zurückbringen.
+      Gemeinsam für ein freies offenes und unzensiertes Internet. Soziale Medien waren zwar noch nie meins doch ich probiere mal Rauten: #selfhostB4itstolate #freenet
+      <br />
+      Hoffe ich kann mit der Webseite ein wenig mehr Einblick dahingehend bieten, auch nehme ich mir diese Webseite als Zukunftsexperiment vor.
+      Wie lange hält die Seite der weiterentwicklung des Webs aus, wie viel ändere ich mich in den Jahren mit Design, Wind- u. Wetter, technischen Dingen.
+      <br />
+      Ich füge hier mal eine Playlist ein macht es euch gemütlich hört Leute, welche ich abfeiere während ich das nervös und schnell zusammenschreibe für (m)eine Zukunft
+      welche mir selber extrem unsicher und Nebelig erscheint. Täglich versuche ich  mich wie jeder andere Mensch zu verbessern, wo ich kann anderen anzupassen mir selbst
+      so passend zu machen wie ein Künstler sich selber gerecht werden kann ohne sich die Ohren abzuschneiden. Was nicht heißt das ich nicht manchmal etwas komisch und Radikal
+      bin.
+    </p>
+  </main>
+  <footer>
+    @2021 hoffentlich besser. Webseite von Felix Bauerschäfer.
+    Credits hier:
+  </footer>
+</body>
+</html>
+?>
